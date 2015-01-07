20e2e688399972b9583c50997e584aeab1e8cc66
Ryan Thompson <service@aiwebsystems.com>
2015-01-06 23:38:03 -0600

    Clean

--- routes.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-// Admin Routes
-Route::get('admin/blog', 'Streams\Addon\Module\Blog\Controller\Admin\PostsController@index');
-Route::any('admin/blog/create', 'Streams\Addon\Module\Blog\Controller\Admin\PostsController@create');
-
-// Public Routes
-Route::get('blog', 'Streams\Addon\Module\Blog\Controller\PostsController@index');
