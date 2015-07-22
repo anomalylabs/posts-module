7255067f90c926793c011f260bfd41b858952c8f
Ryan Thompson <service@aiwebsystems.com>
2015-07-22 00:11:34 -0500

    Designate addon

--- resources/routes.php
+++ resources/routes.php
@@ -1,7 +1,7 @@
 <?php
 
-Route::any('posts', 'Anomaly\PostsModule\Http\Controller\PostsController@index');
-Route::any('posts/tag/{tag}', 'Anomaly\PostsModule\Http\Controller\TagsController@index');
-Route::any('posts/preview/{id}', 'Anomaly\PostsModule\Http\Controller\PostsController@preview');
-Route::any('posts/category/{category}', 'Anomaly\PostsModule\Http\Controller\CategoriesController@index');
-Route::any('posts/{year}/{month}/{day}/{post}', 'Anomaly\PostsModule\Http\Controller\PostsController@show');
+Route::any('posts', ['uses' => 'Anomaly\PostsModule\Http\Controller\PostsController@index', 'streams::addon' => 'anomaly.module.posts']);
+Route::any('posts/tag/{tag}', ['uses' => 'Anomaly\PostsModule\Http\Controller\TagsController@index', 'streams::addon' => 'anomaly.module.posts']);
+Route::any('posts/preview/{id}', ['uses' => 'Anomaly\PostsModule\Http\Controller\PostsController@preview', 'streams::addon' => 'anomaly.module.posts']);
+Route::any('posts/category/{category}', ['uses' => 'Anomaly\PostsModule\Http\Controller\CategoriesController@index', 'streams::addon' => 'anomaly.module.posts']);
+Route::any('posts/{year}/{month}/{day}/{post}', ['uses' => 'Anomaly\PostsModule\Http\Controller\PostsController@show', 'streams::addon' => 'anomaly.module.posts']);
