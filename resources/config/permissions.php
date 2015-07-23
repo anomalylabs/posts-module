3f25768f424020384aaf6599db5b92b06887f118
Brennon Loveless <brennon.loveless@gmail.com>
2015-07-22 23:28:02 -0600

    Add permissions and language

---
+++ resources/config/permissions.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+    'posts'      => [
+        'read',
+        'write',
+        'delete',
+        'write_live',
+        'make_live'
+    ],
+    'categories' => [
+        'read',
+        'write',
+        'delete'
+    ],
+    'types'      => [
+        'read',
+        'write',
+        'delete',
+        'fields'
+    ],
+    'fields'     => [
+        'read',
+        'write',
+        'delete'
+    ]
+];
---
+++ resources/lang/en/permission.php
@@ -0,0 +1,31 @@
+<?php
+
+return [
+    'posts'      => [
+        'title'      => 'Posts',
+        'read'       => 'Can read posts?',
+        'write'      => 'Can write posts?',
+        'delete'     => 'Can delete posts?',
+        'write_live' => 'Can write live posts?',
+        'make_live'  => 'Can make draft posts live?'
+    ],
+    'categories' => [
+        'title'  => 'Categories',
+        'read'   => 'Can read categories?',
+        'write'  => 'Can create and edit categories?',
+        'delete' => 'Can delete categories?'
+    ],
+    'types'      => [
+        'title'  => 'Types',
+        'read'   => 'Can read types?',
+        'write'  => 'Can write types?',
+        'delete' => 'Can delete types?',
+        'fields' => 'Can modify fields of types?'
+    ],
+    'fields'     => [
+        'title'  => 'Fields',
+        'read'   => 'Can read fields?',
+        'write'  => 'Can write fields?',
+        'delete' => 'Can delete fields?'
+    ]
+];
