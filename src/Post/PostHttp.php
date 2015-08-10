e62e115d952b90e8a4e1ebef9aa7b27cb31a62bd
Ryan Thompson <service@aiwebsystems.com>
2015-08-10 02:54:52 -0500

    Better cache

--- src/Post/PostHttp.php
+++ src/Post/PostHttp.php
@@ -58,13 +58,12 @@ class PostHttp
 
         if ($ttl && $seconds = $ttl * 60) {
 
-            $response->headers->set('Pragma', 'public');
             $response->headers->set('Content-Type', 'text/html');
+            $response->headers->set('Cache-Controle', 'must-revalidate');
             $response->headers->set('Etag', md5(json_encode($post->toArray())));
-            $response->headers->set('Cache-Control', 'public,max-age=' . $seconds . ',s-maxage=' . $seconds);
             $response->headers->set(
                 'Last-Modified',
-                $post->lastModified()->setTimezone('GMT')->format('D, d M Y H:i:s')
+                $post->lastModified()->setTimezone('GMT')->format('D, d M Y H:i:s T')
             );
 
             $response->setTtl($seconds);
