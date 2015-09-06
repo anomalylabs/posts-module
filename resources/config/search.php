309efb6cc5028f5b4b260763449a76bbcc49c77d
Ryan Thompson <service@aiwebsystems.com>
2015-09-06 02:27:52 -0500

    Config for saerch

---
+++ resources/config/search.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'Anomaly\PostsModule\Post\PostModel' => [
+        'default' => [
+            'enabled' => 'live',
+            'fields'  => [
+                'title'            => 'title',
+                'summary'          => 'summary',
+                'meta_title'       => 'meta_title',
+                'meta_description' => 'meta_description',
+                'meta_keywords'    => 'meta_keywords'
+            ]
+        ]
+    ]
+];
