37c8679b01e6e0849e9473b2f3d4b402059f5414
Jack Sun <sunhsilu@gmail.com>
2015-11-24 15:47:41 +0800

    Add Chinese traditional translation.

---
+++ resources/lang/zh-tw/tab.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'organization' => '組織頁籤',
+    'permalinks'   => '固定網址',
+    'advanced'     => '進階',
+    'general'      => '一般',
+    'display'      => '顯示',
+    'options'      => '選項',
+    'fields'       => '欄位',
+    'layout'       => '佈局',
+    'post'         => 'Post',
+    'seo'          => 'SEO',
+    'css'          => 'CSS',
+    'js'           => 'JS'
+];
