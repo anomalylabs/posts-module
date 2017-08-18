b9b6316d683d0235a0cd13fc4fc0d76e41b9b545
Ryan Thompson <ryan@pyrocms.com>
2017-08-18 13:07:37 -0500

    Doc

--- migrations/2017_08_18_175445_anomaly.module.posts__remove_meta_keywords_field.php
+++ migrations/2017_08_18_175445_anomaly.module.posts__remove_meta_keywords_field.php
@@ -2,6 +2,13 @@
 
 use Anomaly\Streams\Platform\Database\Migration\Migration;
 
+/**
+ * Class AnomalyModulePostsRemoveMetaKeywordsField
+ *
+ * @link   http://pyrocms.com/
+ * @author PyroCMS, Inc. <support@pyrocms.com>
+ * @author Ryan Thompson <ryan@pyrocms.com>
+ */
 class AnomalyModulePostsRemoveMetaKeywordsField extends Migration
 {
 
