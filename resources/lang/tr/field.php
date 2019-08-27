7a55cedf1717aedf2c8bd6abd95e7664e7528907
alp <alp@visiosoft.com.tr>
2019-08-27 17:34:38 +0300

    Turkish translations added

---
+++ resources/lang/tr/field.php
@@ -0,0 +1,126 @@
+<?php
+
+return [
+    "name" => [
+        "name" => "isim",
+        "instructions" => [
+            "types" => "Bu gönderi türü için kısa bir açıklayıcı ad belirtin.        ",
+            "categories" => "Bu kategori için kısa bir açıklayıcı ad belirtin."
+        ],
+
+    ],
+    "title" => [
+        "name" => "Başlık",
+        "instructions" => "Bu yayın için kısa bir açıklayıcı başlık belirtin.",
+
+    ],
+    "slug" => [
+        "name" => "sümüklüböcek",
+        "instructions" => [
+            "types" => "Sümüklü böcek,
+         bu tür gönderiler için veritabanı tablosu oluşturulmasında kullanılır.        ",
+            "categories" => "Sümüklü böcek kategorinin URL'sini oluştururken kullanılır.        ",
+            "posts" => "Sülük,
+         gönderinin URL'sini oluştururken kullanılır."
+        ],
+
+    ],
+    "description" => [
+        "name" => "Açıklama",
+        "instructions" => [
+            "types" => "Yazı tipini kısaca açıklayın.        ",
+            "categories" => "Kategoriyi kısaca tanımlayın."
+        ],
+        "warning" => "Bu,
+      web sitenizin nasıl oluşturulduğuna bağlı olarak genel olarak gösterilebilir veya gösterilmeyebilir.",
+
+    ],
+    "summary" => [
+        "name" => "özet",
+        "instructions" => "Bu gönderiyi tanıtmak için kısa bir özet yazın.",
+
+    ],
+    "category" => [
+        "name" => "Kategori",
+        "instructions" => "Bu gönderinin hangi kategoriye ait olduğunu seçin.",
+
+    ],
+    "meta_title" => [
+        "name" => "Meta Başlığı",
+        "instructions" => "SEO başlığını belirtin.",
+        "warning" => [
+            "posts" => "Gönderi başlığı varsayılan olarak kullanılacaktır.        ",
+            "types" => "Tip adı varsayılan olarak kullanılacaktır.        ",
+            "categories" => "Kategori adı varsayılan olarak kullanılacaktır."
+        ],
+
+    ],
+    "meta_description" => [
+        "name" => "Meta Açıklaması",
+        "instructions" => "SEO açıklamasını belirtin.",
+
+    ],
+    "theme_layout" => [
+        "name" => "Tema düzeni",
+        "instructions" => "<strong>Post düzenini</strong> sarmak için tema düzenini belirleyin.",
+
+    ],
+    "layout" => [
+        "name" => "Gönderi Düzeni",
+        "instructions" => "Düzen,
+      gönderinin içeriğini görüntülemek için kullanılır.",
+
+    ],
+    "tags" => [
+        "name" => "Etiketler",
+        "instructions" => "Yayınınızı başkalarıyla birlikte gruplandırmanıza yardımcı olacak kuruluş etiketlerini belirtin.",
+
+    ],
+    "enabled" => [
+        "name" => "Etkin",
+        "label" => "Bu yayın etkin mi?",
+        "instructions" => "Devre dışı bırakılmışsa,
+      kontrol panelinde güvenli bir önizleme bağlantısına hala erişebilirsiniz.",
+        "warning" => "Bu gönderi <strong>genel</strong> olarak görüntülenmeden önce etkinleştirilmelidir.",
+
+    ],
+    "featured" => [
+        "name" => "Öne çıkan",
+        "label" => "Bu öne çıkan bir yazı mı?",
+        "instructions" => "Öne çıkan yazılar,
+      belirli yayınlara dikkat çekmek için kullanılabilir.",
+        "warning" => "Bu,
+      web sitenizin nasıl oluşturulduğuna bağlı olarak etkili olabilir veya olmayabilir.",
+
+    ],
+    "publish_at" => [
+        "name" => "Yayınlanma Tarihi / Saati",
+        "instructions" => "Bu yayının yayınlanma tarihini / saatini belirtin.",
+        "warning" => "Geleceğe ayarlanmışsa,
+      bu yayın o zamana kadar görünmez.",
+
+    ],
+    "author" => [
+        "name" => "Yazar",
+        "instructions" => "Bu yayının genel olarak görüntülenen yazarını belirtin.",
+
+    ],
+    "status" => [
+        "name" => "durum",
+        "option" => [
+            "live" => "Canlı        ",
+            "draft" => "taslak        ",
+            "scheduled" => "tarifeli"
+        ],
+
+    ],
+    "content" => [
+        "name" => "içerik",
+
+    ],
+    "type" => [
+        "name" => "tip",
+
+    ],
+
+];
---
+++ resources/lang/tr/message.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    "choose_type" => "Ne tür bir yayın oluşturmak istersiniz?",
+    "change_type" => "Ne tür bir yazı kullanmak istersiniz?",
+    "choose_field_type" => "Hangi alan türünü kullanmak istersiniz?",
+    "choose_field" => "Hangi alanı kullanmak istersiniz?",
+    "tagged" => "Tagged :tag",
+    "scheduled" => "tarifeli",
+    "status" => "durum",
+    "live" => "Canlı",
+    "draft" => "taslak"
+];
